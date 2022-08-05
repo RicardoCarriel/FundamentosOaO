afd136e4d7e7585f97ce22fb9ade47724cc66750
Ricardo Carriel <105672953+RicardoCarriel@users.noreply.github.com>
2022-08-05 12:49:42 -0300

    Add files via upload

---
+++ 1/FundamentosOO/FundamentosOO/FundamentosOO1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundamentosOO
+{
+    internal class FundamentosOO1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var pagamento = new Pagamento1();
+            Console.ReadKey();
+        }
+    }
+    public class Pagamento1 : IDisposable
+    {
+        // Garbage Collector
+
+        public Pagamento1()
+        {
+            Console.WriteLine("Iniciando o pagamento");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Finalizando o pagamento");
+        }
+    }
+}
---
+++ 1/FundamentosOO/FundamentosOO/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace FundamentosOO
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tipo de referencia
+            // -> Endereço dos dados
+            /*var pagamentoBoleto = new PagamentoBoleto();
+            pagamentoBoleto.Pagar();
+            pagamentoBoleto.Vencimento = DateTime.Now;
+            pagamentoBoleto.numeroBoleto = "123";*/
+
+            /*var pagamento = new Pagamento();
+            pagamento.DataPagamento = DateTime.Now;
+            Console.ReadKey();*/
+
+        }
+      
+    }
+    // tipos de Classe:
+    // private, protected, internal e public
+    /*private class Pagamento
+    {
+        //variaveis se tornam PROPRIEDADES
+        //DateTime Vencimento;
+
+        //Adress BillingAdress;
+        //funcoes se tornam metodos  
+
+        /*public string NumeroBoleto;
+
+        public DateTime Vencimento { get; set; } // prop + tab
+
+        private DateTime _dataPagamento;
+
+        public DateTime DataPagamento 
+        {
+            get {
+                Console.WriteLine("Lendo o valor");
+                return _dataPagamento; }
+            set {
+                Console.WriteLine("Atribuindo o valor");
+                _dataPagamento = value; }
+        }*/// propfull
+
+        //public int MyProperty { get; private set; } // propg
+        /*
+        public virtual void Pagar(string numero) //virtual para sobreescrever
+        {
+
+        }
+        public void Pagar(string numero, DateTime Vencimento/*, bool pagarAposVencimento = false*/)
+        //{
+
+        //}
+
+        // PRIVATE fica exclusivo para a classe pagamento
+    //}
+    
+    
+    /*class PagamentoBoleto : Pagamento
+    {
+        public string numeroBoleto;
+
+        public override void Pagar()
+        {
+            //regra boleto
+        }
+        public override string ToString()
+        {
+            return Vencimento.ToString("dd/M/yy");
+        }
+    }
+
+    class PagamentoCartaoCredito : Pagamento
+    {
+        public string numero;
+
+        public override void Pagar()
+        {
+            //regra cartao
+        }
+    }*/
+}
